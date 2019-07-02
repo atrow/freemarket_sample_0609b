c5422a709b338d428896a21e607cd99c1d84343d
OtakeHikaru <yellowtake59@icloud.com>
2019-07-02 14:56:52 +0900

    fixed conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :products, only: [:index, :new, :create, :edit, :update] do
+    resources :product_pictures, only: [:new, :create, :edit, :update]
+  end
 end
