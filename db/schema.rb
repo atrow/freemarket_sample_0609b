6830d6b2e01eab08bd7c86a2751997edacade3f0
OtakeHikaru <yellowtake59@icloud.com>
2019-07-11 08:41:19 +0900

    登録時不要テーブル　削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_112634) do
+ActiveRecord::Schema.define(version: 2019_07_10_233134) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "brand", null: false
@@ -102,9 +102,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_112634) do
     t.string "first_name", null: false
     t.string "first_name_kana", null: false
     t.string "encrypted_password", default: "", null: false
-    t.date "birthday", null: false
     t.bigint "prefecture_id", null: false
-    t.integer "gender", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
