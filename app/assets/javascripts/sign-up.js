1e5d12e5fb299668cb54a79b7727cf1480328403
toexam <tomoyo.omae@gmail.com>
2019-07-28 14:38:19 +0900

    ユーザー新規登録画面

---
+++ app/assets/javascripts/sign-up.js
@@ -0,0 +1,31 @@
+
+window.addEventListener('DOMContentLoaded', function(){
+  //エラーのため追記https://pisuke-code.com/jquery-is-not-defined-solution///
+
+   $('.show-progress__box').hide();//お届け先住所入力プログレスバー
+   $('.done-progress__box').hide();//登録完了プログレスバー
+   $('.hidden-field').hide();
+   $('.sign-up--hidden').hide();
+   $('.form-group__label--hidden').hide();//お届け先住所登録ラベル
+   $('.sign-up-submit-box').hide();//次へ進む２登録
+
+ $('.button-next').on('click',function(){
+
+        $('h1').text('お届け先住所入力');
+        $('.show-progress__box').show();//お届け先住所入力プログレスバー
+        $('.hidden-progress__box').hide();//新規会員登録プログレスバー
+        $('.done-progress__box').hide();//新規会員登録プログレスバー
+//        $('.form-group').hide();//フィールド# TODO An invalid form control with name='' is not focusable.エラーを解決後、非表示に戻す
+        $('.recaptcha').hide();//フィールド
+        $('.label-text').hide();//フィールド
+        $('.form-group__label--hidden').show();//お届け先住所登録ラベル
+        $('.hidden-field').show();//フィールド
+        $('.sign-up-next-box').hide();
+        $('.sign-up--hidden').show();
+        $('.sign-up-submit-box').show();//次へ進む１
+    })
+
+    $('.button-ragistration').on('click',function(){
+      $('.form-group').show();//フィールド
+    })
+  });
