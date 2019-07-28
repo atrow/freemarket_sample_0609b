6250ae3368f59ae7ff198db84d861fbfdacafb84
toexam <tomoyo.omae@gmail.com>
2019-07-28 14:49:18 +0900

    色ボタンなど設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  get "users/new" => "users#new"
+  resources :users, only: [:show, :new, :create, :edit, :update]
   root 'products#index'
   resources :products, only: [:index, :new, :create, :edit, :update] do
     resources :images, only: [:new, :create, :edit, :update]
