68823c86f0ea19264592754512986840e59324c1
toexam <tomoyo.omae@gmail.com>
2019-08-17 11:20:17 +0900

    指摘取り込み

--- app/assets/javascripts/sign-up.js
+++ app/assets/javascripts/sign-up.js
@@ -1,27 +1,30 @@
-
 window.addEventListener('DOMContentLoaded', function(){
-   $('.show-progress__box').hide();//お届け先住所入力プログレスバー
-   $('.done-progress__box').hide();//登録完了プログレスバー
-   $('.hidden-field').hide();
-   $('.sign-up--hidden').hide();
-   $('.form-group__label--hidden').hide();//お届け先住所登録ラベル
-   $('.sign-up-submit-box').hide();//次へ進む２登録
+  $('.show-progress__box').hide();//お届け先住所入力プログレスバー
+  $('.done-progress__box').hide();//登録完了プログレスバー
+  $('.hidden-field').hide();//.form-group--hidden
+  $('.hidden-field').hide();
+  $('.form-group-delivery').hide();//お届け先住所登録ラベル
+  $('.form-group-hidden').hide();//お届け先住所登録ラベル
+  $('.form-group-label--hidden').hide();//お届け先住所登録ラベル必須
+  $('.button-registration').hide();//次へ進む２登録
 
- $('.button-next').on('click',function(){
-        $('h1').text('お届け先住所入力');
-        $('.show-progress__box').show();//お届け先住所入力プログレスバー
-        $('.hidden-progress__box').hide();//新規会員登録プログレスバー
-        $('.done-progress__box').hide();//新規会員登録プログレスバー
-        $('.recaptcha').hide();//フィールド
-        $('.label-text').hide();//フィールド
-        $('.form-group__label--hidden').show();//お届け先住所登録ラベル
-        $('.hidden-field').show();//フィールド
-        $('.sign-up-next-box').hide();
-        $('.sign-up--hidden').show();
-        $('.sign-up-submit-box').show();//次へ進む１
-    })
+  $('.button-next').on('click',function(){
+    $('h1').text('お届け先住所入力');
+    $('.show-progress__box').show();//お届け先住所入力プログレスバー
+    $('.hidden-progress__box').hide();//新規会員登録プログレスバー
+    $('.done-progress__box').hide();//新規会員登録プログレスバー
+    $('.recaptcha').hide();//リキャプチャ
+    $('.form-group__label').hide();//ラベル
+    $('.form-group__label--hidden').show();//お届け先住所登録ラベル
+    $('.form-group-delivery').show();//お届け先住所登録ラベルform-group__label
+    $('.form-group-label__require').show();
+    $('.hidden-field').show();//フィールド
+    $('.sign-up-next-box').hide();
+    $('.sign-up--hidden').show();
+    $('.button-registration').show();//次へ進む１
+  })
 
-    $('.button-ragistration').on('click',function(){
-      $('.form-group').show();//フィールド
+  $('.button-ragistration').on('click',function(){
+    $('.form-group').show();//フィールド
     })
   });
