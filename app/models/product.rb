d650aed24c2cc8afbe79df782c883693df36d10b
Tatumura <cunyuelongye@murakoshitatsuyanoMacBook-puro-2.local>
2019-09-25 12:25:39 +0900

    論理削除
    物理的削除から論理削除に変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ApplicationRecord
   belongs_to :size, optional: true
   belongs_to :brand, optional: true
   belongs_to :product_status
-  has_one :purchase, dependent: :destroy
+  has_one :purchase
   delegate :seller, to: :purchase
   delegate :buyer, to: :purchase
   accepts_nested_attributes_for :purchase
@@ -30,6 +30,8 @@ class Product < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc).limit(4) }
   scope :get_category_products, -> (category_id) { where(category_id: category_id)}
+
+  acts_as_paranoid
 
   def self.get_ladies
     categories = Category.where(parent_id: 14..32)
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,5 @@
 class Purchase < ApplicationRecord
   belongs_to :seller, class_name: 'User', optional: true
   belongs_to :buyer, class_name: 'User', optional: true
-  belongs_to :product, dependent: :destroy
+  belongs_to :product
 end
