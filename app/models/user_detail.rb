0b9ef6ef5b0bb6287e38e6285b43ea8609695fc0
toexam <tomoyo.omae@gmail.com>
2019-10-17 22:03:50 +0900

    addValidatesZipCode7Digit

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,7 +1,7 @@
 class UserDetail < ApplicationRecord
 
   validates :zip_code,:prefecture_id, :city, :street, presence: true
-
+  validates :zip_code,  format: {with: /\A\d{7}\z/}
   belongs_to :user
   belongs_to :prefecture, optional: true
 end
--- spec/models/user_details_spec.rb
+++ spec/models/user_details_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe UserDetail do
-#カラムが全て入力された時登録できる
-  it "is valid with prefecture_id, zip_code, city, street, building_name" do
+  #都道府県・郵便番号・市区町村・番地が入力された時登録できる
+  it "is valid with prefecture_id, zip_code, city, street" do
     user_detail = build(:user_detail)
     expect(user_detail).to be_valid
   end
@@ -16,6 +16,25 @@ describe UserDetail do
     user_detail = build(:user_detail, zip_code: nil)
     user_detail.valid?
     expect(user_detail.errors[:zip_code]).to include("を入力してください")
+  end
+
+ #７桁の郵便番号が入力されていない時は登録できない
+  it "is invalid without a 7-digit zip_code" do
+    user_detail = build(:user_detail, zip_code: "1")
+    user_detail.valid?
+    expect(user_detail.errors[:zip_code]).to include("は不正な値です")
+  end
+
+  it "is invalid without a 7-digit zip_code" do
+    user_detail = build(:user_detail, zip_code: "88888888")
+    user_detail.valid?
+    expect(user_detail.errors[:zip_code]).to include("は不正な値です")
+  end
+  #７桁の郵便番号でもハイフンが入力されている時は登録できない
+  it "is invalid without a 7-digit zip_code" do
+    user_detail = build(:user_detail, zip_code: "888-8888")
+    user_detail.valid?
+    expect(user_detail.errors[:zip_code]).to include("は不正な値です")
   end
 
   it "is invalid without a city" do
